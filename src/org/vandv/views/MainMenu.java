879d884e76bdfebbd5f3a42b465eceefdaf42db9
Vincent Seguin <vinceseguin@gmail.com>
2014-07-03 11:17:05 -0400

    Main menu done.

---
+++ src/org/vandv/views/MainMenu.java
@@ -0,0 +1,17 @@
+package org.vandv.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.vandv.R;
+
+/**
+ * Created by vinceseguin on 03/07/14.
+ */
+public class MainMenu extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_menu);
+    }
+}
