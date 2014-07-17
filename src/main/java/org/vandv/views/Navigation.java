9801a17fc9e05b3245bb64ef5c58ad7ca771d966
Vincent Seguin <vinceseguin@gmail.com>
2014-07-17 16:55:30 -0400

    maps are now loading, TO THE PUBgit status! :)

--- src/main/java/org/vandv/views/Navigation.java
+++ src/main/java/org/vandv/views/Navigation.java
@@ -1,7 +1,12 @@
 package org.vandv.views;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 import org.vandv.R;
 
 /**
@@ -9,11 +14,22 @@ import org.vandv.R;
  */
 public class Navigation extends Activity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //com.google.android.gms.
         setContentView(R.layout.navigation);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return super.onCreateOptionsMenu(menu);
     }
 }
